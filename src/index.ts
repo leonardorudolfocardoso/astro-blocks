8fb271ab3ceb0f418e7205224b54fd10fb0ac7de
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-03-14 12:20:42 -0300

    feat: add footer

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from "./components/Header.astro";
+export { default as Footer } from "./components/Footer.astro";
 export { default as VideoHero } from "./components/VideoHero.astro";
