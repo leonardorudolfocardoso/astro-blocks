8919322d125fcb97d86494d3de99cd47f5132d6a
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-03-14 14:34:07 -0300

    feat: add image hero

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./components/Header.astro";
 export { default as Footer } from "./components/Footer.astro";
 export { default as VideoHero } from "./components/VideoHero.astro";
+export { default as ImageHero } from "./components/ImageHero.astro";
