a25661ec9cd8f4638afd18924ebcffc73b34b83c
Leonardo C. <leonardorudolfocardoso@gmail.com>
2026-03-16 13:13:13 -0300

    feat: add navmenu block

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./components/Header.astro";
 export { default as Footer } from "./components/Footer.astro";
 export { default as VideoHero } from "./components/VideoHero.astro";
 export { default as ImageHero } from "./components/ImageHero.astro";
+export { default as NavMenu } from "./components/NavMenu.astro";
